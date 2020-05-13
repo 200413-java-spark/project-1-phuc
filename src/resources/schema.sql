11e2620697f2fa426ba96b0e457d27e50632a221
Sparce Zissef <phuctqle@gmail.com>
2020-05-12 23:38:58 -0500

    fixing schema again

--- src/resources/schema.sql
+++ src/resources/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE singleCol(
         ID serial primary key,
-        col1 varchar
+        col1 varchar,
         col2 varchar
 );
 
 CREATE TABLE doubleCol (
-        ID serial primary key,,
-        keyName varchar
+        ID serial primary key,
+        keyName varchar,
         valName varchar
 );
 
 CREATE TABLE tripleCol (
         ID serial primary key,
-        keyName varchar
-        valName1 varchar
+        keyName varchar,
+        valName1 varchar,
         valName2 varchar
 );
